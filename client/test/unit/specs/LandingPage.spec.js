05b95e79acc8fcdc66f9e24a3ba0d2a4d2884c23
Eric Mignot <eric.mignot@gmail.com>
2018-10-31 13:57:13 -0700

    more documentation about LandingPage

--- client/test/unit/specs/LandingPage.spec.js
+++ client/test/unit/specs/LandingPage.spec.js
@@ -11,4 +11,41 @@ describe('LandingPage.vue', () => {
         expect(vm.$el.querySelector('h2').textContent)
             .toEqual('Your authorization is missing or has expired. Please login.');
     });
+
+    describe('when authenticated', ()=>{
+
+        let store;
+        let vm;
+        beforeEach(()=>{
+            store = {
+                getters: {
+                    isAuthenticated:true,
+                    statsDataJSON: {
+                        draft: {
+                            response: {
+                                numFound: 3
+                            }
+                        },
+                        hold: {
+                            response: {
+                                numFound: 5
+                            }
+                        }
+                    }
+                }
+            };
+            const Constructor = Vue.extend(LandingPage);
+            vm = new Constructor({store:store}).$mount();
+        })
+        it('renders welcome page', () => {
+            expect(vm.$el.querySelector('h3').textContent).toContain('Welcome to Name X!');
+        })
+        it('displays not examined count', () => {
+            expect(vm.$el.querySelector('.card-body').textContent).toContain('Not Examined: 3');
+        })
+        it('displays hold count', () => {
+            expect(vm.$el.querySelector('.card-body').textContent).toContain('Hold: 5');
+        })
+    })
+
 });
